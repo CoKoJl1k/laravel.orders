b5021863c38256502963dd95347b012d2c03a803
unknown <sergei>
2023-09-14 10:19:41 +0300

    add routes orders

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\OrderController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/requests', [OrderController::class, 'index']);
+Route::put('/requests/{id}',  [OrderController::class, 'update']);
+Route::post('/requests',  [OrderController::class, 'store']);
